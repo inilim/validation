f39177fc4e655565d7e9ab5daaba5a69a6befe36
Dmitry <40324321+inilim@users.noreply.github.com>
2026-01-09 01:00:23 +0300

    ref

--- cli-tools/phpunit.php
+++ cli-tools/phpunit.php
@@ -1,15 +1,22 @@
 <?php
 
 use Inilim\Tool\VD;
+use Inilim\Tool\File;
 use Inilim\Tool\Path;
 use Symfony\Component\Process\Process;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$bin = __DIR__ . '/../vendor/bin/phpunit';
-$bin = Path::realPath($bin);
+$root = __DIR__ . '/../';
+$root = Path::realPath($root);
+$bin = $root . '/vendor/bin/phpunit';
 $bin = Path::normalize($bin);
 $process = new Process(['php', $bin]);
 $process->run();
 $output = $process->getOutput();
+
+$fileOutput = Path::normalize($root . '/files/phpunit-last-output.txt');
+File::put($fileOutput, $output);
+
 echo $output;
+echo PHP_EOL . \sprintf('Output file "%s"', $fileOutput);
